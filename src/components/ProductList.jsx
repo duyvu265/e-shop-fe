1405eb92c0762a5cbc0b1e267e4fe5bb7c292c98
duyvu26 <duyvu26597@gmail.com>
2024-10-16 22:09:33 +0700

    hien thi product

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -20,9 +20,8 @@ const ProductList = ({ categoryId, limit, searchParams }) => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get(`${apiUrl}/products/`);
-        console.log("response",response);
-        
-        setProducts(response?.data?.items);
+        console.log("response", response);
+        setProducts(response?.data);
         setPagination({
           currentPage: response?.data?.currentPage,
           hasPrev: response?.data?.hasPrev,
@@ -33,58 +32,59 @@ const ProductList = ({ categoryId, limit, searchParams }) => {
       }
     };
 
-    if (categoryId || searchParams) {
-      fetchProducts();
-    }
-  }, [categoryId, limit, searchParams, apiUrl ,]);
+    fetchProducts();
+  }, [categoryId, limit, searchParams, apiUrl]);
 
   useEffect(() => {
     const sortProducts = (products) => {
-      return products.sort((a, b) => a.price.price - b.price.price); 
+      return products.sort((a, b) => a.price - b.price);
     };
 
     setSortedProducts(sortProducts([...products]));
   }, [products]);
-  
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      {sortedProducts.map((product) => (
-        <Link
-          to={"/" + product?.slug}
-          className="w-full flex flex-col gap-4 sm:w-[40%] lg:w-[18%]"
-          key={product?._id}
-        >
-          <div className="relative w-full h-64">
-            <img
-              src={product?.media?.mainMedia?.image?.url || "/product.png"}
-              alt={product?.name}
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500 w-full h-full"
-            />
-            {product?.media?.items && (
+      {Array.isArray(sortedProducts) && sortedProducts.length > 0 ? (
+        sortedProducts.map((product) => (
+          <Link
+            to={"/" + product?.slug}
+            className="w-full flex flex-col gap-4 sm:w-[40%] lg:w-[18%]"
+            key={product?._id}
+          >
+            <div className="relative w-full h-64">
               <img
-                src={product?.media?.items[1]?.image?.url || "/product.png"}
+                src={product?.media?.mainMedia?.image?.url || "/product.png"}
                 alt={product?.name}
-                className="absolute object-cover rounded-md w-full h-full"
+                className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500 w-full h-full"
               />
-            )}
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">{product?.name}</span>
-            <span className="font-semibold">${product?.price?.price}</span>
-          </div>
-          {product?.additionalInfoSections && (
-            <div className="text-sm text-gray-500">
-              {product?.additionalInfoSections.find(
-                (section) => section.title === "shortDesc"
-              )?.description || ""}
+              {product?.media?.items && (
+                <img
+                  src={product?.media?.items[0]?.image?.url || "/product.png"} 
+                  alt={product?.name}
+                  className="absolute object-cover rounded-md w-full h-full"
+                />
+              )}
             </div>
-          )}
-          <button className="rounded-2xl ring-1 ring-[#F35C7A] text-[#F35C7A] w-max py-2 px-4 text-xs hover:bg-[#F35C7A] hover:text-white">
-            Add to Cart
-          </button>
-        </Link>
-      ))}
+            <div className="flex justify-between">
+              <span className="font-medium">{product?.name}</span>
+              <span className="font-semibold">${product?.price}</span>
+            </div>
+            {product?.additionalInfoSections && (
+              <div className="text-sm text-gray-500">
+                {product?.additionalInfoSections.find(
+                  (section) => section.title === "shortDesc"
+                )?.description || ""}
+              </div>
+            )}
+            <button className="rounded-2xl ring-1 ring-[#F35C7A] text-[#F35C7A] w-max py-2 px-4 text-xs hover:bg-[#F35C7A] hover:text-white">
+              Add to Cart
+            </button>
+          </Link>
+        ))
+      ) : (
+        <p>No products found.</p>
+      )}
       {searchParams?.cat || searchParams?.name ? (
         <Pagination
           currentPage={pagination?.currentPage}
